037caa0b77f3f05f6565665e38d2b45f722eaa03
TikitakaMike83 <manzomichele83@gmail.com>
2022-02-28 17:30:16 +0100

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  title = 'Angular';
+  title2 = 'Angular1'; 
+  title3 = 'Angular2'; 
+  title4 = 'Angular3'; 
+}

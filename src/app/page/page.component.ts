466746e5862d42e9deaf338b4bb62ac50b5c3e77
TikitakaMike83 <manzomichele83@gmail.com>
2022-03-08 13:05:41 +0100

    3 files modified

--- src/app/page/page.component.ts
+++ src/app/page/page.component.ts
@@ -19,7 +19,8 @@ books = [
     isbn:'88454-4',
     trama: 'loren',
     page : 500,
-    letto : 'OK'
+    letto : 'OK',
+    voto: 4
   },
   {
     titolo: 'Il nome della Rosa2',
@@ -28,7 +29,8 @@ books = [
     isbn:'88454-42',
     trama: 'loren2',
     page : 500,
-    letto : 'NO'
+    letto : 'NO',
+    voto : 3
   },   
 ];
   constructor() { }
